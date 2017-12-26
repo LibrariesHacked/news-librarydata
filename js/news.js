966429199fc3aa7c8aae1c4b6007e650dbc835d6
Dave <DaveBathnes@users.noreply.github.com>
2017-12-26 22:24:17 +0000

    adding charts

--- js/news.js
+++ js/news.js
@@ -1,18 +1,35 @@
 var News = {
+    stories: [],
+    type_month_counts: [],
+    type_year_counts: [],
     load: function (callback) {
         $.get('/data/PLNStories.json', function (data) {
             var stories = [];
+            var yr = {}, mth = {};
             $.each(Object.keys(data), function (i, type) {
+                yr[type] = {};
+                mth[type] = {};
                 $.each(Object.keys(data[type]), function (y, year) {
+                    yr[type][mth] = 0;
                     $.each(Object.keys(data[type][year]), function (z, month) {
+                        mth[type][year + month] = 0;
                         $.each(data[type][year][month], function (idx, story) {
-                            story.push(type);
+                            // Change the date format
+                            story[1] = moment(story[1], 'ddd, DD MMM YYYY HH:mm:ss +0000').format('YYYYMMDD');
+                            yr[type][year]++;
+                            yr[type][year + month]++; 
+                            var type_description = 'News';
+                            if (type === 'changes') type_description = 'Change';
+                            story.push(type_description);
                             stories.push(story);
                         });
-                    });
-                });
-            });
+                    }.bind(this));
+                }.bind(this));
+            }.bind(this));
+            this.type_year_counts = yr;
+            this.type_month_counts = mth;
+            this.stories = stories;
             callback(stories);
-        });
+        }.bind(this));
     }
 };
--- js/site.js
+++ js/site.js
@@ -6,12 +6,67 @@
             data: data,
             columns: [
                 { title: 'Where' },
-                { title: 'Date' },
-                { title: 'Story', className: 'none' },
-                { title: 'Actions' },
+                {
+                    title: 'Date',
+                    render: function (data, type, row) {
+                        if (type === 'sort') {
+                            return data;
+                        } else {
+                            return moment(data).format('ddd Do MMM YYYY');
+                        }
+                    }
+                },
+                { title: 'News', className: 'none' },
+                {
+                    title: 'Actions',
+                    render: function (data, type, row) {
+                        if (type === 'sort') {
+                            return data;
+                        } else {
+                            return '<a title="Go to Public Libraries News" target="_blank" href="http://www.publiclibrariesnews.com/' + data + '"><i class="fa fa-external-link" aria-hidden="true"></i></a>';
+                        }
+                    }
+                },
                 { title: 'Type' }
             ],
-            order: [1, 'asc']
+            order: [1, 'desc']
+        });
+
+        var ctx = document.getElementById('cht_types_per_year').getContext('2d');
+        var line = new Chart(ctx, {
+            type: 'bar',
+            data: Object.keys(News.type_year_counts).map(function (i, type) {
+                return {
+                    labels: Object.keys(News.type_year_counts[type]),
+                    data: [],
+                    backgroundColor: [
+                        'rgba(255, 99, 132, 0.2)',
+                        'rgba(54, 162, 235, 0.2)',
+                        'rgba(255, 206, 86, 0.2)',
+                        'rgba(75, 192, 192, 0.2)',
+                        'rgba(153, 102, 255, 0.2)',
+                        'rgba(255, 159, 64, 0.2)'
+                    ],
+                    borderColor: [
+                        'rgba(255,99,132,1)',
+                        'rgba(54, 162, 235, 1)',
+                        'rgba(255, 206, 86, 1)',
+                        'rgba(75, 192, 192, 1)',
+                        'rgba(153, 102, 255, 1)',
+                        'rgba(255, 159, 64, 1)'
+                    ],
+                    borderWidth: 1
+                };
+            }),
+            options: {
+                scales: {
+                    yAxes: [{
+                        ticks: {
+                            beginAtZero: true
+                        }
+                    }]
+                }
+            }
         });
     });
 });
