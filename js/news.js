d604c80e6887955c9a1f972e10af7269acedcf16
Dave <dave_roweuk@yahoo.co.uk>
2017-12-27 11:27:11 +0000

    working on top counts

--- js/news.js
+++ js/news.js
@@ -2,10 +2,11 @@ var News = {
     stories: [],
     type_month_counts: [],
     type_year_counts: [],
+    location_counts: [],
     load: function (callback) {
         $.get('/data/PLNStories.json', function (data) {
             var stories = [];
-            var yr = {}, mth = {};
+            var yr = {}, mth = {}, locations = {};
             $.each(Object.keys(data), function (i, type) {
                 yr[type] = {};
                 mth[type] = {};
@@ -15,6 +16,8 @@ var News = {
                         mth[type][year + month] = 0;
                         $.each(data[type][year][month], function (idx, story) {
                             // Change the date format
+                            if (!locations[story[1]]) locations[story[1]] = 0;
+                            locations[story[1]] = locations[story[1]] + 1;
                             story[1] = moment(story[1], 'ddd, DD MMM YYYY HH:mm:ss +0000').format('YYYYMMDD');
                             yr[type][year]++;
                             yr[type][year + month]++; 
@@ -26,6 +29,10 @@ var News = {
                     }.bind(this));
                 }.bind(this));
             }.bind(this));
+            $.each(Object.keys(locations), function (i, loc) {
+              this.location_counts.push([loc, locations[loc]]);
+            }.bind(this));
+            this.location_counts = this.location_counts.sort(function(a, b){ return b[1] - a[1]})
             this.type_year_counts = yr;
             this.type_month_counts = mth;
             this.stories = stories;
