83f4cc1c49a1432a63ff85d38e83f67e1211cb52
dave_roweuk <dave_roweuk@yahoo.co.uk>
2017-12-28 17:59:54 -0500

    Adding leaflet and working on filters

--- js/news.js
+++ js/news.js
@@ -1,53 +1,70 @@
 var News = {
-    years: [],
-    types: [],
-    selected_years: [2017, 2018],
-    selected_types: ['changes', 'local']
-    stories: [],
-    type_month_counts: [],
-    type_year_counts: [],
-    location_counts: [],
-    load: function (callback) {
-        $.when($.get('/data/PLNStories.json'), $.get('/data/Locations.json'))
-          .done(function (story_data, location_data) {
-            var stories = [];
-            var yr = {}, mth = {}, locations = {};
-            $.each(Object.keys(story_data), function (i, type) {
-                this.types.push(type);
-                yr[type] = {};
-                mth[type] = {};
-                $.each(Object.keys(story_data[type]), function (y, year) {
-                    this.years.push(year);
-                    yr[type][mth] = 0;
-                    $.each(Object.keys(story_data[type][year]), function (z, month) {
-                        mth[type][year + month] = 0;
-                        $.each(story_data[type][year][month], function (idx, story) {
-                            // Change the date format
-                            if (!locations[story[1]]) locations[story[1]] = 0;
-                            locations[story[1]] = locations[story[1]] + 1;
-                            $.each(location_data, function(){
-                              
-                            });
-                            story[1] = moment(story[1], 'ddd, DD MMM YYYY HH:mm:ss +0000').format('YYYYMMDD');
-                            yr[type][year]++;
-                            yr[type][year + month]++; 
-                            var type_description = 'News';
-                            if (type === 'changes') type_description = 'Change';
-                            story.push(type_description);
-                            stories.push(story);
-                        });
-                    }.bind(this));
-                }.bind(this));
+  story_data: {},
+  location_data: {},
+  years: [],
+  types: [],
+  selected_years: [2017, 2018],
+  selected_types: ['changes', 'local'],
+  stories: [],
+  type_month_counts: [],
+  type_year_counts: [],
+  location_counts: [],
+  load: function(callback) {
+    $.when($.get('/data/PLNStories.json'), $.get('/data/PLNLocations.json'))
+      .done(function(story_data, location_data) {
+        this.story_data = story_data;
+        this.location_data = location_data;
+        this.process();
+        callback(this.stories);
+      }.bind(this));
+  },
+  process: function() {
+    var stories = [];
+    var yr = {},
+      mth = {},
+      locations = {};
+    $.each(Object.keys(this.story_data), function(i, type) {
+      this.types.push(type);
+      yr[type] = {};
+      mth[type] = {};
+      if (this.selected_types.indexOf(type) !== -1) {
+        $.each(Object.keys(this.story_data[type]), function(y, year) {
+          this.years.push(year);
+          yr[type][mth] = 0;
+          if (this.selected_years.indexOf(year) !== -1) {
+            $.each(Object.keys(this.story_data[type][year]), function(z, month) {
+              mth[type][year + month] = 0;
+              $.each(this.story_data[type][year][month], function(idx, story) {
+                // Change the date format
+                if (!locations[story[1]]) locations[story[1]] = [0,0,0];
+                locations[story[1]][0] = locations[story[1]][0] + 1;
+                $.each(Object.keys(this.location_data), function(y, loc) {
+                  if (loc === story[1]) {
+                    locations[story[1]][1] = this.location_data[loc][0];
+                    locations[story[1]][2] = this.location_data[loc][1];
+                  }
+                });
+                story[1] = moment(story[1], 'ddd, DD MMM YYYY HH:mm:ss +0000').format('YYYYMMDD');
+                yr[type][year]++;
+                yr[type][year + month]++;
+                var type_description = 'News';
+                if (type === 'changes') type_description = 'Change';
+                story.push(type_description);
+                stories.push(story);
+              });
             }.bind(this));
-            $.each(Object.keys(locations), function (i, loc) {
-              this.location_counts.push([loc, locations[loc]]);
-            }.bind(this));
-            this.location_counts = this.location_counts.sort(function(a, b){ return b[1] - a[1]})
-            this.type_year_counts = yr;
-            this.type_month_counts = mth;
-            this.stories = stories;
-            callback(stories);
+          }
         }.bind(this));
-    },
-    process: function(){}
+      }
+    }.bind(this));
+    $.each(Object.keys(locations), function(i, loc) {
+      this.location_counts.push([loc, locations[loc]]);
+    }.bind(this));
+    this.location_counts = this.location_counts.sort(function(a, b) {
+      return b[1] - a[1]
+    })
+    this.type_year_counts = yr;
+    this.type_month_counts = mth;
+    this.stories = stories;
+  }
 };
--- js/site.js
+++ js/site.js
@@ -1,94 +1,107 @@
-﻿$(function () {
+$(function() {
 
-    // Set up Data Table
-    $('#tbl-news').DataTable({
-            responsive: true,
-            data: [],
-            dom: 'Bfrtip',
-            buttons: ['copy', 'excel', 'pdf'],
-            columns: [
-                { title: 'Where' },
-                {
-                    title: 'Date',
-                    render: function (data, type, row) {
-                        if (type === 'sort') {
-                            return data;
-                        } else {
-                            return moment(data).format('ddd Do MMM YYYY');
-                        }
-                    }
-                },
-                { title: 'News', className: 'none' },
-                {
-                    title: 'Actions',
-                    render: function (data, type, row) {
-                        if (type === 'sort') {
-                            return data;
-                        } else {
-                            return '<a title="Go to Public Libraries News" target="_blank" href="http://www.publiclibrariesnews.com/' + data + '"><i class="fa fa-external-link" aria-hidden="true"></i></a>';
-                        }
-                    }
-                },
-                { title: 'Type' }
-            ],
-            order: [1, 'desc']
-    });
-    
-    // Set up line chart
-    var ctx = document.getElementById('cht_types_per_year').getContext('2d');
-        var line = new Chart(ctx, {
-            type: 'bar',
-            data: Object.keys(News.type_year_counts).map(function (i, type) {
-                return {
-                    labels: Object.keys(News.type_year_counts[type]),
-                    data: [],
-                    backgroundColor: [
-                        'rgba(255, 99, 132, 0.2)',
-                        'rgba(54, 162, 235, 0.2)',
-                        'rgba(255, 206, 86, 0.2)',
-                        'rgba(75, 192, 192, 0.2)',
-                        'rgba(153, 102, 255, 0.2)',
-                        'rgba(255, 159, 64, 0.2)'
-                    ],
-                    borderColor: [
-                        'rgba(255,99,132,1)',
-                        'rgba(54, 162, 235, 1)',
-                        'rgba(255, 206, 86, 1)',
-                        'rgba(75, 192, 192, 1)',
-                        'rgba(153, 102, 255, 1)',
-                        'rgba(255, 159, 64, 1)'
-                    ],
-                    borderWidth: 1
-                };
-            }),
-            options: {
-                scales: {
-                    yAxes: [{
-                        ticks: {
-                            beginAtZero: true
-                        }
-                    }]
-                }
-            }
-        });
-        
-        
-    var setDataTable = function() {
-    
-    
-    
-    };
-    
-    
-    var setLine = function() {};
-    
-    
-    
-    
-    News.load(function (data) {
-    
-        // On initial load load in the data with the custom filters
+  var map = L.map('map').setView([51.505, -0.09], 13);
+  L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}', {
+    attribution: 'Map data &copy; <a href="http://openstreetmap.org">OpenStreetMap</a> contributors, <a href="http://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, Imagery © <a href="http://mapbox.com">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox.streets',
+    accessToken: 'pk.eyJ1IjoibGlicmFyaWVzaGFja2VkIiwiYSI6IlctaDdxSm8ifQ.bxf1OpyYLiriHsZN33TD2A'
+  }).addTo(mymap);
+  // Set up Data Table
+  $('#tbl-news').DataTable({
+    responsive: true,
+    data: [],
+    dom: 'Bfrtip',
+    buttons: ['copy', 'excel', 'pdf'],
+    columns: [{
+        title: 'Where'
+      },
+      {
+        title: 'Date',
+        render: function(data, type, row) {
+          if (type === 'sort') {
+            return data;
+          } else {
+            return moment(data).format('ddd Do MMM YYYY');
+          }
+        }
+      },
+      {
+        title: 'News',
+        className: 'none'
+      },
+      {
+        title: 'Actions',
+        render: function(data, type, row) {
+          if (type === 'sort') {
+            return data;
+          } else {
+            return '<a title="Go to Public Libraries News" target="_blank" href="http://www.publiclibrariesnews.com/' + data + '"><i class="fa fa-external-link" aria-hidden="true"></i></a>';
+          }
+        }
+      },
+      {
+        title: 'Type'
+      }
+    ],
+    order: [1, 'desc']
+  });
 
-        
-    });
+  // Set up line chart
+  var ctx = document.getElementById('cht_types_per_year').getContext('2d');
+  var line = new Chart(ctx, {
+    type: 'bar',
+    data: Object.keys(News.type_year_counts).map(function(i, type) {
+      return {
+        labels: Object.keys(News.type_year_counts[type]),
+        data: [],
+        backgroundColor: [
+          'rgba(255, 99, 132, 0.2)',
+          'rgba(54, 162, 235, 0.2)',
+          'rgba(255, 206, 86, 0.2)',
+          'rgba(75, 192, 192, 0.2)',
+          'rgba(153, 102, 255, 0.2)',
+          'rgba(255, 159, 64, 0.2)'
+        ],
+        borderColor: [
+          'rgba(255,99,132,1)',
+          'rgba(54, 162, 235, 1)',
+          'rgba(255, 206, 86, 1)',
+          'rgba(75, 192, 192, 1)',
+          'rgba(153, 102, 255, 1)',
+          'rgba(255, 159, 64, 1)'
+        ],
+        borderWidth: 1
+      };
+    }),
+    options: {
+      scales: {
+        yAxes: [{
+          ticks: {
+            beginAtZero: true
+          }
+        }]
+      }
+    }
+  });
+
+
+  var setDataTable = function() {
+
+
+
+  };
+
+
+  var setLine = function() {};
+
+
+
+
+  News.load(function(data) {
+
+    // On initial load load in the data with the custom filters
+
+
+  });
 });
